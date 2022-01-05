dabaf3d03b2d6a16519acff5b61ab6b6c0991ec3
Zakaria <zsumonn@gmail.com>
2022-01-05 16:47:56 +0600

    added link

--- components/Layout.js
+++ components/Layout.js
@@ -8,14 +8,14 @@ const Layout = ({ children }) => {
 			<Head>
 				<title>Paynux</title>
 				<meta
+					property='og:image'
+					content='https://i.ibb.co/bP1ymdx/pynux-logo.png'
+				/>
+				<meta
 					name='description'
 					content='Paynux is a platform for businesses to manage their finances.'
 				/>
 				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
-				<meta
-					property='og:image'
-					content='https://i.ibb.co/bP1ymdx/pynux-logo.png'
-				/>
 			</Head>
 			<Navbar />
 			{children}

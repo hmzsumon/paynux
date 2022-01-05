d9da4a2d3f03b17d566e94d8711839f3deb02734
Zakaria <zsumonn@gmail.com>
2022-01-05 14:43:51 +0600

    added Head

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import React from 'react';
 import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
 	return (
 		<>
+			<Head>
+				<title>Paynux</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+			</Head>
 			<Navbar />
 			{children}
 		</>
